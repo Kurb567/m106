b628cdf96eb2783aea23066afa769cdec7597be1
Kurb567 <kurbanali.magomedov567@gmail.com>
2026-03-22 17:57:35 +0300

    1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "8557116313:AAEqp_YBnxLfXZX9VVP5Dtg5XtRZFvIySgw"
-MARZBAN_URL = "https://ctjkk.duckdns.org:8000/"
+MARZBAN_URL = "https://ctjkk.duckdns.org:8000"
 MARZBAN_USER = "admin"
 MARZBAN_PASS = "56731096842"
 PHOTO_PATH = "1.jpg"  # Путь к файлу фото
